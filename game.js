d6a76b84ab0d373db7a5b563b725d2c3b396a971
kyle martin <kylemartin98@outlook.com>
2020-06-19 12:49:03 +0100

    Destiny Quiz

--- game.js
+++ game.js
@@ -117,3 +117,29 @@ let questions = [
 
     },
 ]
+
+const CORRECT_BONUS = 15;
+const MAX_QUESTIONS = 8;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [ ...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+};
+
+
+startGame();
