efb84b239b3a93ec68cede02449b995b0afedeb0
kyle martin <kylemartin98@outlook.com>
2020-06-25 16:10:48 +0100

    Destiny Quiz

---
+++ end.js
@@ -0,0 +1,10 @@
+/*const username = document.getElementsByName("Username");
+
+username.addEventListener ('keyup', () => {
+    console.log(username.value);
+});
+
+saveHighScore = e => {
+   // console.log("clicked save");
+    e.preventDefault();
+};
