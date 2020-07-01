38fbe162aa76833d9af7419d824ea40aab668562
kyle martin <kylemartin98@outlook.com>
2020-07-01 15:08:16 +0100

    Destiny Quiz part3

--- end.js
+++ end.js
@@ -1,10 +1,14 @@
-/*const username = document.getElementsByName("Username");
-
+const username = document.getElementById('Username');
+const saveScore = document.getElementById('saveScore');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
 username.addEventListener ('keyup', () => {
     console.log(username.value);
+    saveScore.disabled = !username.value;
 });
 
 saveHighScore = e => {
-   // console.log("clicked save");
+    console.log("clicked save");
     e.preventDefault();
 };
